2541940c4900a1df79474f878c3fbfb75cfb3191
Julio Trecenti <julio.trecenti@gmail.com>
2017-10-13 14:35:57 -0300

    load musics from music21 corpus

---
+++ R/download.R
@@ -0,0 +1,27 @@
+#' Get list of compositions
+#'
+#' Return all filenames in the corpus that match a composer's or a
+#' collection's name.
+#'
+#' @param composer a character, composer or collection name, e.g. \code{bach}.
+#' @param file_ext if provided, defines which extensions are returned.
+#'   \code{file_ext=NULL} (default) returns all extensions.
+#'
+#' @export
+#' @examples {
+#' get_composer("schoenberg")
+#' }
+get_composer <- function(composer, file_ext = NULL) {
+  music21$corpus$getComposer(composer, file_ext)
+}
+
+#' Load music from work name
+#'
+#' Loads music from music21 internal path
+#'
+#' @param path character internal path, e.g. \code{bach/bwv66.6}
+#'
+#' @export
+load_music <- function(path) {
+  music21$corpus$parse(path)
+}
